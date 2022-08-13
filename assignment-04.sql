5a61f1713cfb1654ddf092b6718e45b4c36fa038
Beytullah BİLEK <54630694+beytomer@users.noreply.github.com>
2022-08-13 23:01:04 +0300

    Create assignment-04.sql

---
+++ assignment-04.sql
@@ -0,0 +1,39 @@
+/*
+	Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+  
+  Q1) film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+  
+  Q2) film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+  
+  Q3) film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+  
+  Q4) country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+  
+  Q5) city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+*/
+
+--Answer 1
+
+SELECT DISTINCT replacement_cost FROM film;
+
+--Answer 2
+
+SELECT COUNT (DISTINCT replacement_cost) FROM film;
+
+--Answer 3 
+
+SELECT  COUNT(title) FROM film
+WHERE  (title LIKE 'T%'AND rating = 'G');
+
+--Answer 4 
+
+SELECT COUNT(country) FROM country
+WHERE country LIKE '_____'; 
+
+--Answer 5
+
+SELECT COUNT(city) FROM city
+WHERE city ILIKE '%r';
+
+
+
