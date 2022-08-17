2ec8f5f4a45065077c30c10c55147103f2800dcd
Beytullah BİLEK <54630694+beytomer@users.noreply.github.com>
2022-08-17 23:42:51 +0300

    Create assignment-09.sql

---
+++ assignment-09.sql
@@ -0,0 +1,28 @@
+/*
+
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+Q-1) city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+Q-2) customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz 
+INNER JOIN sorgusunu yazınız.
+
+Q-3) customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz 
+INNER JOIN sorgusunu yazınız.
+*/
+
+--Answer 1
+
+SELECT city,country FROM city
+INNER JOIN country ON city.country_id = country.country_id;
+
+--Answer 2
+
+SELECT payment_id,first_name,last_name FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id;
+
+--Answer 3
+
+SELECT rental_id,first_name,last_name FROM customer
+INNER JOIN rental ON customer.customer_id=rental.customer_id;
+
